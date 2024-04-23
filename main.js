82fe3ac690e51aab16dd4bc85abb3eba35b44ef3
1443501 <106714590+1443501@users.noreply.github.com>
2024-04-22 20:32:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ rightWristX="";
 rightWristY="";
 leftWristX="";
 leftWristY="";
+score_l=0;
+score_r=0;
+song1_status="";
+song2_status="";
 
  function preload(){
     song1=loadSound("music.mp3");
@@ -26,10 +30,37 @@ leftWristY="";
 
  function draw(){
     image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("white");
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    if(score_l>0.2)
+{
+   circle(leftWristX,leftWristY,20);
+   if(song1_status==true){
+      song1.stop();
+      song2.play();
+      document.getElementById("song_name").innerHTML= "Song 1 is playing.";
+      
+   }
+
+   if(score_r>0.2){
+      cicle(rightWristX,rightWristY,20);
+      if(song2_status==true){
+         song1.play();
+      song2.stop();
+   document.getElementById("song_name").innerHTML="Song 2 is playing.";
+      }
+   }
+  
+}
+
+    
  }
 function gotPoses(results){
    if(results.length > 0){
       console.log(results);
+      score_l=results[0].pose.leftWrist[9].score;
 
       rightWristX=results[0].pose.rightWrist.x;
       rightWristY=results[0].pose.rightWrist.y;
